83c9c7999fdfa8f28fc12793fd15161fb4811d56
Elity <xing.suifeng@gmail.com>
2018-04-03 16:35:02 +0800

    命令分组

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.runMcok", function() {
       if (running) return;
